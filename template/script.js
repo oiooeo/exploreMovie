940666bc3398e975007aca8bff67bd3b6401a43b
Choi Yoonseo <choiys1103@naver.com>
2023-05-30 12:05:19 +0900

    파일 이름 수정

---
+++ template/script.js
@@ -0,0 +1,14 @@
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+  },
+};
+
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
