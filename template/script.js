b9ce718a6f55a512aa260659e06fe16230d66e08
Choi Yoonseo <choiys1103@naver.com>
2023-05-31 12:04:26 +0900

    Feat: API 불러와서 카드 형식으로 띄우기, 카드 클릭 이벤트 추가, CSS 수정

--- template/script.js
+++ template/script.js
@@ -2,13 +2,50 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTU4YzgzNGRjNWU5ZTk4MmNkNWU5YzYyMWQ3ZjEzYiIsInN1YiI6IjY0NzA5ZWM1NTQzN2Y1MDBjMzI4MzY3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ohD6y4xS5i0GFnbWHvCuUNrNPvenh2u3bCJLYPxKSA0",
   },
 };
+
+let resultArray = [];
 
 fetch(
   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
   options
 )
   .then((response) => response.json())
-  .then((response) => console.log(response))
+  .then((response) => {
+    response.results.forEach((movie) => {
+      resultArray.push({
+        id: movie.id,
+        title: movie.title,
+        img: movie.poster_path,
+        desc: movie.overview,
+        rate: movie.vote_average,
+      });
+
+      let id = movie["id"];
+      let title = movie["title"];
+      let img = movie["poster_path"];
+      let desc = movie["overview"];
+      let rate = movie["vote_average"];
+
+      let temp_html = `<div class="movie-card" onclick="alert('${title}의 영화 ID는 ${id} 입니다');">
+                        <img
+                          class="movie_img"
+                          src="https://image.tmdb.org/t/p/original${img}"
+                          alt=""
+                        />
+                        <p class="movie_name"><b>${title}</b></p>
+                        <p class="movie_desc">${desc}</p>
+                        <p class="movie_rate">평점 | <b>${rate}</b></p>
+                      </div>`;
+
+      document
+        .querySelector("#movies")
+        .insertAdjacentHTML("beforeend", temp_html);
+    });
+  })
   .catch((err) => console.error(err));
+
+console.log(resultArray);
